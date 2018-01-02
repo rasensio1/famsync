c77d6edabacd393cf6794fa67289c7197735e3dd
Ryan Asensio <ryan@Ryans-MacBook-Air.local>
2018-01-02 11:28:16 -0600

    Add model validations for Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  validates :text, :category_id, presence: true
+  has_one :category
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_item
+    Item.new({text: "Clean house",
+              done: false,
+              category_id: "1"})
+
+  end
+  test "is valid" do
+    assert valid_item.valid?
+  end
+
+  test "invalid without text" do
+    inval = valid_item
+    inval.text = ''
+    assert inval.invalid?
+  end
+
+  test "invalid without category" do
+    inval = valid_item
+    inval.category_id = nil
+    assert inval.invalid?
+  end
 end
